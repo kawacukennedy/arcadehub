b8064a9b131ec399bc249b3eda5647c0bc46fcc6
kawacukennedy <kawacukent@gmail.com>
2025-10-16 18:14:13 +0200

    Add CI workflow, README, skeleton server test

---
+++ server/src/test/java/com/arcadehub/server/ServerMainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServerMainTest {
+
+    @Test
+    public void testServerStart() {
+        // TODO: Implement integration test
+        assertTrue(true);
+    }
+}
